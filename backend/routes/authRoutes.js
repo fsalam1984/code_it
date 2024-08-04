14d320d0fd6fe6963a18492186077169ae55594a
Aiyana Scarborough <aiyanascar@Aiyanas-Laptop.attlocal.net>
2024-08-04 10:46:05 -0400

    fix changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const authController = require('../controllers/authController');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/logout', authController.logout);
+router.post('/register', authController.register);
 
 module.exports = router;
