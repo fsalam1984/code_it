9070bcdb8fb072a9608327c60babf6903e0709d0
Aiyana Scarborough <aiyanascar@Aiyanas-Laptop.attlocal.net>
2024-08-04 10:55:38 -0400

    Resolved merge conflicts

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config(); // Make sure this is at the top
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -26,6 +27,7 @@ mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true, // Add this line
   })
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.log(err));
