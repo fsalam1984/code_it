1c920e2e7a2bacd1ff28958c1c029f952df549a7
faisalsalam <fsenterprise1@gmail.com>
2024-08-04 12:23:36 -0400

    updates to components and css files

---
+++ code_connect/client/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/Navbar.css'
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+         <div className='navbar-left'>
+
+            <Link to="/" className='navbar-brand'> Code Connect</Link>
+        </div>
+        <div className='right-navbar'>
+           
+            <Link to="/profile" className='navbar-link'>Profile</Link>
+            <Link to="/login" className='navbar-link'>Login</Link>
+    
+
+        </div>
+    </nav>
+   
+  )
+}
+
+export default Navbar
