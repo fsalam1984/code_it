a64ec9944990987fb73a85806bfd24521006d122
Aiyana Scarborough <aiyanascar@Aiyanas-Laptop.attlocal.net>
2024-08-05 11:26:34 -0400

    updates

--- backend/controllers/searchController.js
+++
@@ -1,7 +0,0 @@
-// controllers/searchController.js
-
-// Dummy function for search profiles
-exports.searchProfiles = (req, res) => {
-    // This is a placeholder function. Faisal implement search logic here.
-    res.send('Search profiles functionality is not implemented yet.');
-};
--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-
-router.post('/', profileController.createOrUpdateProfile);
-router.get('/:userId', profileController.getProfile);
-router.delete('/:userId', profileController.deleteProfile);
-
-module.exports = router;
