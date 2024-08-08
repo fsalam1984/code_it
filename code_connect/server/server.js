431b701ef4fb3ae226840aabec4e02757d715d76
faisalsalam <fsenterprise1@gmail.com>
2024-08-07 20:32:19 -0400

    updates to server.js for mongo connection

--- code_connect/server/server.js
+++ code_connect/server/server.js
@@ -1,34 +1,45 @@
 // server.js
+require('dotenv').config(); // Make sure this is at the top
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config(); // Make sure this is at the top
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3009;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
+
 const authRoutes = require('../db/routes/authRoutes');
+const friendRoutes = require('../db/routes/friendRoutes');
+const profileRoutes = require('../db/routes/profileRoutes');
 const searchRoutes = require('../db/routes/searchRoutes');
+const testRoutes = require('../db/routes/testRoute');
+
 // Add more routes as needed
 
 app.use('/api/auth', authRoutes);
+app.use('/api/', searchRoutes);
 app.use('/api/search', searchRoutes);
+
+app.use('/api/search', searchRoutes);
+
+
 // Add more routes as needed
 
 // Database connection
-mongoose.connect('mongodb://127.0.0.1:27017/code-connect');
+// mongoose.connect('mongodb://127.0.0.1:27017/code-connect');
 
 mongoose
-  .connect(MONGO_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true, // Add this line
+    // useCreateIndex: true, // Add this line
   })
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.log(err));
