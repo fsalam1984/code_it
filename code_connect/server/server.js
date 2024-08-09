ae279b98d7d5c1688f8b78e16640ff6c15c8601e
faisalsalam <fsenterprise1@gmail.com>
2024-08-08 20:31:50 -0400

    added npm i config

--- code_connect/server/server.js
+++ code_connect/server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config(); // keep this at the top
+const db = require('./db/config/db');
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -17,24 +18,26 @@ const authRoutes = require('./db/routes/authRoutes');
 const friendRoutes = require('./db/routes/friendRoutes');
 const profileRoutes = require('./db/routes/profileRoutes');
 const searchRoutes = require('./db/routes/searchRoutes');
-const testRoutes = require('./db/routes/testRoute');
+// const testRoutes = require('./db/routes/testRoute');
 
 // Add more routes as needed
 app.use('/api/auth', authRoutes);
 app.use('/api/friends', friendRoutes); 
 app.use('/api/profile', profileRoutes);
 app.use('/api/search', searchRoutes);
-app.use('/api/test', testRoutes);
+// app.use('/api/test', testRoutes);
 
-// Database connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected...'))
-  .catch((err) => console.log(err));
+// // Database connection
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('MongoDB connected...'))
+//   .catch((err) => console.log(err));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
