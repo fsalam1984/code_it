c0792a4dc88a096a08136869be3476fd512c21ad
chan <chandlerdaniel254@gmail.com>
2024-08-10 13:14:29 -0400

    changes

--- code_connect/client/src/components/Home.jsx
+++ code_connect/client/src/components/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import '../css/Home.css'
+import Friends from './Friends'
 const Home = () => {
 
   return (
-    <div className='hero'>
-      <h1 className='hero-text'></h1>
-      <p className='hero-description'> This is the profile section where the user profile info is displayed ..........etc....
-      </p>
-   
+    <>
+      <div className='hero'>
+        <h1 className='hero-text'></h1>
+        <p className='hero-description'> This is the profile section where the user profile info is displayed ..........etc....
+        </p>
+        <div className='hero-image'> </div>
+      <Friends />
+      </div>
 
-      <div className='hero-image'> </div>
-    </div>
 
+    </>
 
   )
 }
