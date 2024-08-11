99fc0df396542a504fee24bde3ac0e7b1c99114e
chan <chandlerdaniel254@gmail.com>
2024-08-11 18:58:05 -0400

    resolve mutation.js

--- code_connect/client/src/utils/mutations.js
+++ code_connect/client/src/utils/mutations.js
@@ -1,76 +1,30 @@
 import { gql } from '@apollo/client';
-
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-    user {
-      _id
-      email
-      username
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}
 `;
-
 export const ADD_FRIEND = gql`
-<<<<<<< HEAD
-mutation addFriend($friendId: ID!) {
-  addFriend(friendId: $friendId) {
-    _id
-    email
-    friends {
-      _id
-    }
-    username
-    profile {
-      bio
-      years_of_experience
-      companies
-      job_title
-      education
-      languages
-      niche
-      unique_characteristic
-      images
-=======
   mutation addFriend($friendId: ID!) {
     addFriend(friendId: $friendId) {
       _id
       email
       username
->>>>>>> 6efdf7e62d22f51d9d4af4635c19e4f47a59b996
     }
   }
 `;
-
 export const REMOVE_FRIEND = gql`
-<<<<<<< HEAD
-mutation removeFriend($friendId: ID!) {
-  removeFriend(friendId: $friendId) {
-    _id
-    email
-    username
-    friends {
-      _id
-    }
-    profile {
-      bio
-      years_of_experience
-      companies
-      job_title
-      education
-      languages
-      niche
-      unique_characteristic
-      images
-=======
   mutation removeFriend($friendId: ID!) {
     removeFriend(friendId: $friendId) {
       _id
       email
       username
->>>>>>> 6efdf7e62d22f51d9d4af4635c19e4f47a59b996
     }
   }
 `;
