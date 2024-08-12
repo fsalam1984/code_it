49d517e955d18257a33df923e6d6bfaedb4dd7ef
faisalsalam <fsenterprise1@gmail.com>
2024-08-11 23:24:37 -0400

    added package.json for prod render

--- code_connect/server/server.js
+++ code_connect/server/server.js
@@ -1,3 +1,51 @@
+// const express = require('express');
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// // Create a new instance of an Apollo server with the GraphQL schema
+// const startApolloServer = async () => {
+//   await server.start();
+
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(express.json());
+
+//   app.use('/graphql', expressMiddleware(server, {
+//     context: authMiddleware
+//   }));
+
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//     });
+//   }
+
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// // Call the async function to start the server
+//   startApolloServer();
+
+
+//Production Render :
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
@@ -14,7 +62,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
 
@@ -25,14 +72,19 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
+  // Serve static files from the client build directory
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
+  } else {
+    // For development, you might want to serve client files differently
+    app.use(express.static(path.join(__dirname, '../client/public')));
   }
 
+  // Connect to the database and start the server
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -42,4 +94,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
