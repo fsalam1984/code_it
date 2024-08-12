e0ad0da627a122f34cdbf618129a16c2e123fb24
faisalsalam <fsenterprise1@gmail.com>
2024-08-12 15:43:38 -0400

    updates to server.js file

--- code_connect/server/server.js
+++ code_connect/server/server.js
@@ -43,26 +43,80 @@
 
 // // Call the async function to start the server
 //   startApolloServer();
-import path from 'path'
-import {fileURLToPath, filteURLToPath} from 'url'
+// const { fileURLToPath } = require('url');
+// const path = require('path');
+// //Resolving dirname for ES module
+// const _filename = fileURLToPath(new URL(import.meta.url));
+// const __dirname = path.dirname(_filename)
 
-//Resolving dirname for ES module
-const _filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(_filename)
+// //Production Render :
+// const express = require('express');
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
 
-//Production Render :
+// const { authMiddleware } = require('./utils/auth');
+
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const startApolloServer = async () => {
+//   await server.start();
+
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(express.json());
+
+//   app.use('/graphql', expressMiddleware(server, {
+//     context: authMiddleware
+//   }));
+
+//   // Serve static files from the client build directory
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+//     });
+//   } else {
+//     // For development, you might want to serve client files differently
+//     app.use(express.static(path.join(__dirname, '../client/public')));
+//   }
+
+//   // Connect to the database and start the server
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// // Call the async function to start the server
+// startApolloServer();
+
+
+//////////////////
+const path = require('path');
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+// Resolving __dirname in CommonJS (you don't need fileURLToPath for CommonJS)
+// const __dirname = path.resolve(); // This gives you the root directory of your project, which should work for most cases.
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 const server = new ApolloServer({
   typeDefs,
@@ -76,7 +130,7 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
+    context: authMiddleware,
   }));
 
   // Serve static files from the client build directory
