69a4ea1bf4dbfdd40adca2f962cd7517e5d57ec3
fsalam1984 <161408871+fsalam1984@users.noreply.github.com>
2024-08-12 23:44:26 -0400

    Update PotentialFriends.jsx

--- code_connect/client/src/components/PotentialFriends.jsx
+++ code_connect/client/src/components/PotentialFriends.jsx
@@ -1,74 +1,56 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
-import '../css/PotentialFriends.css'
-import { useMutation } from '@apollo/client';
-import { useQuery } from '@apollo/client';
-import { ADD_FRIEND } from '../utils/mutations';
-import { REMOVE_FRIEND } from '../utils/mutations';
-import { QUERY_ALL_USERS } from '../utils/queries';
-import { QUERY_ME } from '../utils/queries';
-const PotentialFriends = () => {
+import '../css/PotentialFriends.css';
+import { useMutation, useQuery } from '@apollo/client';
+import { ADD_FRIEND, REMOVE_FRIEND } from '../utils/mutations';
+import { QUERY_ALL_USERS, QUERY_ME } from '../utils/queries';
 
-  // const {loading, error, data} = useMutation(QUERY_ALL_USERS)
+const PotentialFriends = () => {
     const [addFriend] = useMutation(ADD_FRIEND);
     const [deleteFriend] = useMutation(REMOVE_FRIEND);
-    const {data, loading} = useQuery(QUERY_ALL_USERS)
+    const { data, loading } = useQuery(QUERY_ALL_USERS);
     
-    console.log(data)
-   
-  if (loading){
-    return (
-      <div>loading</div>
-    )
-  }
-    // Dummy friend data for display
-    // const friends = [
-    //   { id: '1', name: 'Jack Smith', imgSrc: '/Images/user1.jpg' },
-    //   { id: '2', name: 'Mike Brown', imgSrc: '/Images/user2.jpg' },
-    //   { id: '3', name: 'Jill Stone', imgSrc: '/Images/user3.jpg' },
-    // ];
-    const friends = data?.users
-    // Function to handle adding a friend
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        );
+    }
+
+    const friends = data?.users;
+
     const handleAddFriend = async (id) => {
-      try {
-        await addFriend({ variables: { friendId:  id } });
-        window.location.assign('/friends')
-        // Optionally, handle success feedback here
-      } catch (error) {
-        console.log("Error adding friend:", error);
-      }
+        try {
+            await addFriend({ variables: { friendId: id } });
+            window.location.assign('/friends');
+        } catch (error) {
+            console.log("Error adding friend:", error);
+        }
     };
 
-      // Function to handle deleting a friend
-  const handleDeleteFriend = async (id) => {
-    try {
-      await deleteFriend({ variables: { friendId: id } });
-      // Optionally, handle success feedback here
-    } catch (error) {
-      console.log("Error deleting friend:", error);
-    }
-  };
+    const handleDeleteFriend = async (id) => {
+        try {
+            await deleteFriend({ variables: { friendId: id } });
+        } catch (error) {
+            console.log("Error deleting friend:", error);
+        }
+    };
 
-  return (
-    
-    <div>
-    <div className="friends-list">
-      <h2>Potential Friends</h2>
-      <ul>
-        {friends.map(friend => (  
-          <li key={friend.id}>
-            <img src={friend.profile.images[0]} alt={friend.name} />
-            <span>{friend.username}</span>
-            <span>{friend.profile.bio}</span>
-            <span>{friend.profile.education}</span>
-            <button onClick={() => handleAddFriend(friend._id)}>Add Friend</button>
-            {/* <button onClick={() => handleDeleteFriend(friend._id)}>Remove Friend</button> */}
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-  )
+    return (
+        <div className="container-profile">
+            <div className="friends-container">
+                {friends.map(friend => (
+                    <div key={friend.id} className="friend-box">
+                        <img src={friend.profile.images[0]} alt={friend.name} />
+                        <h3>{friend.username}</h3>
+                        <p>{friend.profile.bio}</p>
+                        <p>{friend.profile.education}</p>
+                        <button onClick={() => handleAddFriend(friend._id)} className="add-friend-button">
+                            Add Friend
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default PotentialFriends;
